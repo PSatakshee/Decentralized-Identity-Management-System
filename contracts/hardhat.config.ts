ea4943edb7dd009b9792e7cb693f52dfdb33f43a
PSatakshee <sataksheep11@gmail.com>
2025-05-05 20:22:05 +0530

    commit

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -2,15 +2,11 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from "dotenv";
 import "@nomicfoundation/hardhat-viem"
-import { createPublicClient, http, Block } from "viem";
-import { sepolia } from "viem/chains";
+
 
 dotenv.config(); // Load environment variables
 const ALCHEMY_API_URL = process.env.ALCHEMY_API_URL || "";
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
-// if (!PRIVATE_KEY || PRIVATE_KEY.length !== 32) {
-//   throw new Error("❌ Invalid PRIVATE_KEY. It must be a 32-character string.");
-// }
 
 
 const config: HardhatUserConfig = {
@@ -29,40 +25,9 @@ const config: HardhatUserConfig = {
     sepolia: {
       url: ALCHEMY_API_URL || "",
       accounts: [PRIVATE_KEY], 
-      // accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
   },
 };
 
 export default config;
 
-
-// const config: HardhatUserConfig = {
-//   solidity: {
-//     version: "0.8.28", // Explicit version
-//     settings: {
-//       optimizer: { enabled: true, runs: 200 },
-//     },
-//   },
-//   defaultNetwork: "sepolia",
-//   networks: {
-//     hardhat: {},
-//     sepolia: {
-//       url: process.env.ALCHEMY_API_URL || "", // Fallback to empty string if undefined
-//       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [], // Avoids errors if undefined
-//     },
-//   },
-// };
-
-// const client = createPublicClient({
-//   chain: sepolia,
-//   transport: http("https://eth-sepolia.g.alchemy.com/v2/hBL5WNCxvAx6kmnP11dFxY6j9c_aBa7C"),
-// });
-
-// const block: Block = await client.getBlock({
-//   blockNumber: 123456n,
-// });
-
-// console.log(block);
-
-// export default config;
